d975115307bdccc5a2eb1f3e40ed0eea36a85836
esthcarelle <esthcarelle@gmail.com>
2019-09-09 12:27:14 +0200

    add   conditions to classes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,33 +12,38 @@ public class App {
             System.out.println("Do you want to decrypt or encrypt?Write encrypt if you want to encrypt , decrypt for decrypting and exit for exiting");
             String decryptOrEncrypt = myObj.nextLine();
             if(decryptOrEncrypt.equals("exit")){
+                System.out.println("thank you for joining us");
                 programRunning=false;
             }
-            System.out.println("Enter the plaintext please");
-            String stringLength = myObj.nextLine();
-            String stringToLower = stringLength.toLowerCase();
-            inputtedStringInChar = stringToLower.toCharArray();
+            else
+            {
+                System.out.println("Enter the plaintext please");
+                String stringLength = myObj.nextLine();
+                String stringToLower = stringLength.toLowerCase();
+                inputtedStringInChar = stringToLower.toCharArray();
 
-            System.out.println("Enter the key please");
-            String intLength = myObj.nextLine();
-            int n = Integer.parseInt(intLength);
+                System.out.println("Enter the key please");
+                String intLength = myObj.nextLine();
+                int n = Integer.parseInt(intLength);
 
 
-            if (decryptOrEncrypt.equals("encrypt")) {
-                Encrypt myStringToEncrypt = new Encrypt(inputtedStringInChar, n);
-                myStringEncrypted = myStringToEncrypt.EncryptInput();
-                System.out.println(myStringEncrypted);
-            } else if (decryptOrEncrypt.equals("decrypt")) {
-                Decrypt myStringToDecrypt = new Decrypt(inputtedStringInChar, n);
-                myStringEncrypted = myStringToDecrypt.DecryptInput();
-                System.out.println(myStringEncrypted);
-            }
-            else if(decryptOrEncrypt.equals("exit")){
-                programRunning=false;
+                if (decryptOrEncrypt.equals("encrypt")) {
+                    Encrypt myStringToEncrypt = new Encrypt(inputtedStringInChar, n);
+                    myStringEncrypted = myStringToEncrypt.EncryptInput();
+                    System.out.println("Your encrypted  string is:"+ myStringEncrypted);
+                } else if (decryptOrEncrypt.equals("decrypt")) {
+                    Decrypt myStringToDecrypt = new Decrypt(inputtedStringInChar, n);
+                    myStringEncrypted = myStringToDecrypt.DecryptInput();
+                    System.out.println("Your decrypted string is:"+ myStringEncrypted);
+                }
+                else if(decryptOrEncrypt.equals("exit")){
+                    programRunning=false;
 
-            }
+                }
                 else {
-                System.out.println("invalid entry");
+                    System.out.println("invalid entry");
+                }
+
             }
 
 
